a434cccbf095f2d1a8bf0d2b2c97dda60f5dccbe
SZWDSBZTMS <2930053627@qq.com>
2026-07-08 21:52:10 +0800

    Update 02.streamlit.py

--- 02.streamlit.py
+++ 02.streamlit.py
@@ -1,11 +1,8 @@
-from operator import length_hint
-
 import streamlit as st
 
 # 大标题
 st.title("陈都灵")
 st.header("陈都灵天下第一")
-
 st.subheader("陈都灵世一萌")
 
 #段落文字:
@@ -13,11 +10,11 @@ st.write("<左耳>")
 st.write("医学专家说,左耳靠近心脏,甜言蜜语要说给左耳听,如果有个人,对我的左耳说甜言蜜语,即使听不到也没关系")
 
 #图像
-st.image(image="./新建文件夹/resource/Screenshot_20260703_010621.jpg",width=300)
-st.video("./新建文件夹/resource/video_20260703_003031.mp4")
-st.logo("./新建文件夹/resource/IMG_20260706_125910.jpg")
+st.image(image="./资源/Screenshot_20260703_010621.jpg",width=300)
+st.image(image="./资源/Screenshot_20260703_004812.jpg",width=300)
+st.image(image="./资源/Screenshot_20260703_004906.jpg",width=300)
 
 #视频
-st.video("./resource/video_20260703_003031.mp4")
+st.video("./资源/video_20260703_003031.mp4")
 #logo
-st.logo("./resource/IMG_20260706_125910.jpg")
+st.logo("./资源/IMG_20260706_125910.jpg")
